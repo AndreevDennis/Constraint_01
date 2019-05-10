4caecd15dbf539dbfa47e637c540a352a47ae375
Dennis <denisandreev1611@gmail.com>
2019-05-10 21:06:59 +0300

    firstConst

--- Constraint_01/ViewController.swift
+++ Constraint_01/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var square : UIView = {
+        var square = UIView()
+        square.frame  = CGRect(x: 0, y: 0, width: 20, height: 20)
+        square.backgroundColor = .red
+        square.translatesAutoresizingMaskIntoConstraints = false
+        return square
+    }()
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.addSubview(square)
+        creatingSquareConstraints()
     }
-
+    
+    func creatingSquareConstraints(){
+        NSLayoutConstraint(item: square,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: self.view,
+                           attribute: .leadingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: square,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: self.view,
+                           attribute: .trailingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: square,
+                           attribute: .topMargin,
+                           relatedBy: .equal,
+                           toItem: self.view,
+                           attribute: .topMargin,
+                           multiplier: 1,
+                           constant: 88).isActive = true
+        NSLayoutConstraint(item: square,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: square,
+                           attribute: .width,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+    }
 
 }
 
